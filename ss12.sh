bf1201418aa7f6d00ffcf72bf0155bf95801b8d5
AbdelhadyGamal <ag1160@fayoum.edu.eg>
2019-01-12 14:35:52 +0200

    video 28 #if then fi in action

--- ss12.sh
+++ ss12.sh
@@ -1,8 +1,7 @@
 #if- then  statement in action
 echo "Enter source and target file names."
-read source target 
-if mv $source $target 
+read source target
+if mv $source $target
 then
-echo "your file name has been successfully renamed"
+echo "your file name has been successfully renamed."
 fi 
-
